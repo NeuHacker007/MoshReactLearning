9e7d629e0888efcddd34623b372030ffb5d614d0
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-07 11:11:59 -0400

    -- using state to preserve the data used in this component

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <h1>{this.formatCounter()}</h1>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCounter() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
