e7f98ec4e49e00b8df2d5d325cef9f70a158594a
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-07 16:35:01 -0400

    -- handle event

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -2,28 +2,38 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    tags: [],
+    tags: ["tag1", "tag2", "tag3"],
   };
   styles = {
     fontSize: 20,
     fontWeight: "bold",
   };
-  renderTags() {
-    if (this.state.tags.length === 0) return "Please create a new tag";
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          // if we use map to re-map the array data, we need to provide
-          // unique key attribute. Otherwise the react virtual dom will not
-          // not know which item in the list is changing. This is similar
-          // to the trackBy in Angular
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  handleIncrement() {
+    console.log("Counter Increased");
   }
   render() {
-    return <div>{this.renderTags()}</div>;
+    return (
+      <div>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCounter()}
+        </span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCounter() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
