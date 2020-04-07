7b86df92c345e34cd3b422ea3389d8c0ac34838a
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-07 14:15:38 -0400

    -- render a list

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
+    tags: ["tag1", "tag2", "tag3"],
   };
   styles = {
     fontSize: 20,
@@ -9,12 +10,21 @@ class Counter extends Component {
   };
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCounter()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+        <ul>
+          {this.state.tags.map((tag) => (
+            // if we use map to re-map the array data, we need to provide
+            // unique key attribute. Otherwise the react virtual dom will not
+            // not know which item in the list is changing. This is similar
+            // to the trackBy in Angular
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
     );
   }
   getBadgeClasses() {
