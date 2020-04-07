d644c61bc13a5f32a69a3fa4af4f7b1a5596693d
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-07 16:23:07 -0400

    -- conditional rendering

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -2,39 +2,28 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
   styles = {
     fontSize: 20,
     fontWeight: "bold",
   };
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return "Please create a new tag";
     return (
-      <div>
-        <span style={this.styles} className={this.getBadgeClasses()}>
-          {this.formatCounter()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            // if we use map to re-map the array data, we need to provide
-            // unique key attribute. Otherwise the react virtual dom will not
-            // not know which item in the list is changing. This is similar
-            // to the trackBy in Angular
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map((tag) => (
+          // if we use map to re-map the array data, we need to provide
+          // unique key attribute. Otherwise the react virtual dom will not
+          // not know which item in the list is changing. This is similar
+          // to the trackBy in Angular
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-  formatCounter() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  render() {
+    return <div>{this.renderTags()}</div>;
   }
 }
 
