69e5730af073d9c836808ae6abfaf539fda9b4cb
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-18 18:31:07 -0400

    4.3 passing data to components

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ["tag1", "tag2", "tag3"],
   };
   styles = {
@@ -17,25 +17,12 @@ class Counter extends Component {
   handleIncrement = () => {
     //console.log(productId);
     this.setState({
-      count: this.state.count + 1,
+      value: this.state.value + 1,
     });
   };
-  /**
-   * This is one way to passing event paremeter,
-   * It wraps the real handleIncrement function with
-   * another function just because, in
-   *  onClick ={function}
-   * the function here is just the memory address which
-   * cannot have () to pass in the parameter,
-   * An altertive way is just put this dohandleIncrement
-   * replace the function in onClick. To avoid messy by wrapping
-   * too many levels
-   */
-  // doHandleIncrement = () => {
-  //   this.handleIncrement({ id: 1 });
-  // };
 
   render() {
+    console.log("props", this.props);
     return (
       <div>
         <span style={this.styles} className={this.getBadgeClasses()}>
@@ -52,11 +39,11 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
   formatCounter() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
