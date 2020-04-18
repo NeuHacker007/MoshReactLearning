72d6611af0ccc41a7d74f05764c68d844b095ad0
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-18 18:24:03 -0400

    4.2 composing component refactor

--- counter-app/src/components/countersComponent.jsx
+++ counter-app/src/components/countersComponent.jsx
@@ -2,15 +2,21 @@ import React, { Component } from "react";
 import Counter from "./counterComponent";
 
 class Counters extends Component {
-  state = {};
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+      { id: 5, value: 0 },
+    ],
+  };
   render() {
     return (
       <div>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
+        {this.state.counters.map((counter) => {
+          return <Counter key={counter.id} />;
+        })}
       </div>
     );
   }
