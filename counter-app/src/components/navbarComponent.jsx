d80c2320e1b187892ca4d6955324708b3c025281
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-27 15:19:44 -0400

    stateless functional components

--- counter-app/src/components/navbarComponent.jsx
+++ counter-app/src/components/navbarComponent.jsx
@@ -1,17 +1,33 @@
-import React, { Component } from "react";
-class NavBar extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <nav className="navbar navbar-light bg-light">
-          Navbar
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </nav>
-      </React.Fragment>
-    );
-  }
-}
+import React from "react";
+
+const NavBar = (props) => {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-light bg-light">
+        Navbar
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </nav>
+    </React.Fragment>
+  );
+};
 
 export default NavBar;
+
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <nav className="navbar navbar-light bg-light">
+//           Navbar
+//           <span className="badge badge-pill badge-secondary">
+//             {this.props.totalCounters}
+//           </span>
+//         </nav>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+//export default NavBar;
